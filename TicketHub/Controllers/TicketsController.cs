02c354aefc3a41a9e8693fc34b5350f8a2ffd354
Maris Hume <marishume@gmail.com>
2025-03-25 13:21:42 -0300

    Add project files.

---
+++ TicketHub/Controllers/TicketsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketHub;
+
+namespace TicketHub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+
+        private readonly ILogger<TicketsController> _logger;
+        private readonly IConfiguration _configuration;
+
+
+        public TicketsController(ILogger<TicketsController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from the Tickets Controller - GET");
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Post(Ticket contact)
+        {
+
+            //Validation classes: 
+            //https://learn.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations?view=net-9.0
+
+            //This uses the validation inside Contact.cs
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+
+            return Ok("Hello " + contact.name + " from the Tickets Controller - POST");
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ TicketHub/Ticket.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketHub
+{
+    public class Ticket
+    {
+        public int concertId { get; set; } = 0;
+
+        [MaxLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        [Required(ErrorMessage = "Name is required")]
+        public string name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "A Quantity is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero.")]
+        public int quantity { get; set; } = 0;
+
+        [Required(ErrorMessage = "Credit card is required")]
+        [StringLength(16, MinimumLength = 16, ErrorMessage = "Invaild Credit Card Format")]
+        public string creditCard { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Expiration field is required")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/\d{2}$", ErrorMessage = "Expiration date must be in MM/YY format.")]
+        public string expiration { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Security Code is required")]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Invalid security code. It must be 3 or 4 digits.")]
+        public string securityCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Address is required")]
+        public string address { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "City is required")]
+        public string city { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Province is required")]
+        public string province { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        [RegularExpression(@"^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$", ErrorMessage = "Invalid postal code format. Use format A1A 1A1.")]
+        public string postalCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Country is required")]
+        public string country { get; set; } = string.Empty;
+    }
+}
