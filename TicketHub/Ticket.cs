55a51c14311914589a3787ef625e37c9bab2fd37
Maris Hume <marishume@gmail.com>
2025-03-28 10:37:09 -0300

    fixed validation

--- TicketHub/Ticket.cs
+++ TicketHub/Ticket.cs
@@ -19,8 +19,9 @@ namespace TicketHub
         public string phone { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "A Quantity is required")]
-        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than zero.")]
-        public int quantity { get; set; } = 0;
+        [Range(1, 5, ErrorMessage = "Quantity must be between 1 and 5.")]
+        public int quantity { get; set; } = 1;
+
 
         [Required(ErrorMessage = "Credit card is required")]
         [StringLength(16, MinimumLength = 16, ErrorMessage = "Invaild Credit Card Format")]
@@ -32,26 +33,26 @@ namespace TicketHub
 
         [Required(ErrorMessage = "Security Code is required")]
         [RegularExpression(@"^\d{3,4}$", ErrorMessage = "Invalid security code. It must be 3 or 4 digits.")]
-        public string securityCode { get; set; } = "01/26";
+        public string securityCode { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Address is required")]
         [MaxLength(20, ErrorMessage = "Address must be less than 30 characters.")]
-        public string address { get; set; } = "123 Main St";
+        public string address { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "City is required")]
         [MaxLength(20, ErrorMessage = "City must be less than 20 characters.")]
-        public string city { get; set; } = "Halifax";
+        public string city { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Province is required")]
-        [MaxLength(20, ErrorMessage = "Province must be less than 20 characters.")]
-        public string province { get; set; } = "Nova Scotia";
+        [MaxLength(20, ErrorMessage = "Province must be less than 50 characters.")]
+        public string province { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Postal Code is required")]
         [RegularExpression(@"^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$", ErrorMessage = "Invalid postal code format. Use format A1A 1A1.")]
-        public string postalCode { get; set; } = "B3P 1C4";
+        public string postalCode { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Country is required")]
         [MaxLength(20, ErrorMessage = "Country must be less than 20 characters.")]
-        public string country { get; set; } = "Canada";
+        public string country { get; set; } = string.Empty;
     }
 }
